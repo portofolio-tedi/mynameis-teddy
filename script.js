619462f7a5da8f032d342d2222d0bb36dd411ca2
TEDI SUPRIYANTO <teddytri08@gmail.com>
2025-12-18 00:30:26 +0700

    Implement project visibility on scroll
    Add scroll event listener to show projects

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
+const projects = document.querySelectorAll('.project');
+
+window.addEventListener('scroll', () => {
+  projects.forEach(project => {
+    const posisi = project.getBoundingClientRect().top;
+    const tinggiLayar = window.innerHeight;
+
+    if (posisi < tinggiLayar - 50) {
+      project.classList.add('show');
+    }
+  });
+});
